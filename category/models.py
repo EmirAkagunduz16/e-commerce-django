47720ff826f4cdf8b17914299c801f614c3b1f2a
EmirAkagunduz16 <akagunduzemir04@gmail.com>
2025-05-08 14:51:29 +0300

    system is improved

--- category/models.py
+++ category/models.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 
 
 class Category(models.Model):
-    category_name = models.CharField(max_length=200)
+    category_name = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     description = models.TextField(blank=True)
     cat_image = models.ImageField(upload_to='photos/categories', blank=True)
